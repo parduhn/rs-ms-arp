dfa50e3b71daaed896bfa5d179bb44377c4249e7
parduhn <hayo@parduhn.de>
2021-11-14 21:52:15 +0100

    Release

--- deploy_and_start.sh
+++ deploy_and_start.sh
@@ -7,7 +7,7 @@ git push
 cargo build --release
 
 echo "Move build to server. Server password needed."
-rsync -av --progress --exclude='.*' ./target/release/ms_arp hayo@10.1.2.1:/home/hayo/timeover/ms-rust/ms_arp
+rsync -av --recursive --progress --exclude='.*' ./target/release/ms_arp hayo@10.1.2.1:/home/hayo/timeover/ms-rust/ms_arp
 
 cargo set-version --bump patch
 
