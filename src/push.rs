64b1e8e5f44faa08b97ff2d6c4507550763f2ea3
parduhn <hayo@parduhn.de>
2021-11-21 16:00:34 +0100

    Release

--- src/push.rs
+++ src/push.rs
@@ -5,33 +5,29 @@ use pnet::datalink;
 use std::sync::{Arc, Mutex};
 
 pub fn start() {
+    //vector of app states and interfaces
+    let mut app_states: Vec<AppState> = Vec::new();
+
+    for iface in datalink::interfaces() {
+        let interface = iface.to_owned();
+        if interface.is_loopback() || interface.ips.is_empty() {
+            println!("Not using: {:?}", interface);
+        } else {
+            println!("Using: {:?}", interface);
+            let app = AppState {
+                knowns: Arc::new(Mutex::new(ArpResponses {
+                    results: Vec::new(),
+                })),
+                interface,
+            };
+            app_states.push(app);
+        }
+    }
+
     loop {
-        // let interfaces = datalink::interfaces()
-        //     .iter()
-        //     .filter(|ip| !ip.is_loopback() && !ip.ips.is_empty())
-        //     .next()
-        //     .unwrap()
-        //     .to_owned();
         println!("----------------------------------------");
-        for iface in datalink::interfaces() {
-            let interface = iface.to_owned();
-            if interface.is_loopback() || interface.ips.is_empty() {
-                println!("Not using: {:?}", interface);
-            } else {
-                println!("Using: {:?}", interface);
-                let app = AppState {
-                    knowns: Arc::new(Mutex::new(ArpResponses {
-                        results: Vec::new(),
-                    })),
-                    interface: datalink::interfaces()
-                        .iter()
-                        .filter(|ip| !ip.is_loopback() && !ip.ips.is_empty())
-                        .next()
-                        .unwrap()
-                        .to_owned(),
-                };
-                arp_handler(&app)
-            }
+        for app in &app_states {
+            arp_handler(&app)
         }
     }
 }
