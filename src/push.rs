e37ea31a8f640c4426c94ccb839f6b33fc32acc2
parduhn <hayo@parduhn.de>
2021-11-21 14:24:07 +0100

    Release

--- src/push.rs
+++ src/push.rs
@@ -5,18 +5,35 @@ use pnet::datalink;
 use std::sync::{Arc, Mutex};
 
 pub fn start() {
-    let app = AppState {
-        knowns: Arc::new(Mutex::new(ArpResponses {
-            results: Vec::new(),
-        })),
-        interface: datalink::interfaces()
-            .iter()
-            .filter(|ip| !ip.is_loopback() && !ip.ips.is_empty())
-            .next()
-            .unwrap()
-            .to_owned(),
-    };
-    loop {
-        arp_handler(&app)
+    let interfaces = datalink::interfaces()
+        .iter()
+        .filter(|ip| !ip.is_loopback() && !ip.ips.is_empty())
+        .next()
+        .unwrap()
+        .to_owned();
+
+    println!("{:?}", interfaces);
+    println!("----------------------------------------");
+    for iface in datalink::interfaces() {
+        let interface = iface.to_owned();
+        if interfaces.is_loopback() || interfaces.ips.is_empty() {
+            println!("Not using: {:?}", interface);
+        } else {
+            println!("Using: {:?}", interface);
+            let app = AppState {
+                knowns: Arc::new(Mutex::new(ArpResponses {
+                    results: Vec::new(),
+                })),
+                interface: datalink::interfaces()
+                    .iter()
+                    .filter(|ip| !ip.is_loopback() && !ip.ips.is_empty())
+                    .next()
+                    .unwrap()
+                    .to_owned(),
+            };
+            loop {
+                arp_handler(&app)
+            }
+        }
     }
 }
