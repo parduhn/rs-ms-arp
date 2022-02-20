36c4d5a1ff7e28ab9ad89b80a292e4d288cdb209
parduhn <hayo@parduhn.de>
2022-02-20 16:27:58 +0100

    Release

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+service=$(cat Cargo.toml | grep name -m 1 | awk -F' = ' '{ print $2 }' | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
 
 git add .
 git commit -m "Release"
@@ -8,15 +9,15 @@ cargo build --release
 
 echo "Move build to server. Server password needed."
 cd ./target/release
-rsync -avR  --progress --exclude='.*' ms_arp hayo@10.1.2.1:/home/hayo/timeover/services/
+rsync -avR  --progress --exclude='.*' $service hayo@10.1.2.1:/home/hayo/timeover/services/
 cd ../..
 
-rsync -av  --progress --exclude='.*' start_all.sh hayo@10.1.2.1:/home/hayo/timeover/services/
+#rsync -av  --progress --exclude='.*' start_all.sh hayo@10.1.2.1:/home/hayo/timeover/services/
 
 echo "Start service with /timeover/services/start_all.sh"
 
 cargo set-version --bump patch
 
-echo "Service start cargo"
-cp ./start_all_dev.sh ../../timeover/start_all_dev.sh
-chmod a+a ../../timeover/start_all_dev.sh
+#echo "Service start cargo"
+#cp ./start_all_dev.sh ../../timeover/start_all_dev.sh
+#chmod a+a ../../timeover/start_all_dev.sh
